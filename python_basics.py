17f6004949b6077e9b14f2bcb3faa16e79c473f3
Prabu Natarajan <prabuinet@gmail.com>
2019-08-29 19:20:42 +0530

    foo

--- python_basics.py
+++ python_basics.py
@@ -1,18 +1,72 @@
 # -*- coding: utf-8 -*-
 
-"""
 
-Scalar objects in python:
-    
-int
-float
-bool
-NoneType
+#Scalar objects in python:
+
+5       #int
+2.5     #float
+True    #bool
+None    #NoneType
 
 type(5)
 type(3.2)
 type(False)
 type(None)
 
-"""
+#Type Cast
+float(5)
+int(2.5)
+
+
+# operators on ints and floats
+i = 5
+j = 2
+i + j   # sum
+i - j   # difference
+i * j   # product
+i / j   # division
+i // j  # => 2  integer division
+i % j   # => 1  reminder
+i ** j  # => 25 i to the power of j
+
+# operator precedence
+# 1. parantheses
+# 2. **, *, /, + and -
+
+# binding variabls and values
+
+pi = 3.14159
+pi_approx = 22/7
+
+# abstracting expressions
+
+radius = 2.2
+area = pi * (radius ** 2)
+
+
+#comparison operators on int and float
+i >  j
+i >= j
+i <  j
+i <= j
+i == j
+i != j
+
+# a simple program
+x = int(input('enter an integer:'))
+
+if x % 2 == 0:
+    print ('')
+    print ('Even')
+else:
+    print('')
+    print('Odd')
+
+print('Done with conditional')
+
+
+
+
+
+
 
